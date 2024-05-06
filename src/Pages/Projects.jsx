1b50dd945b28f5f7e32e8ef311b93001ab3c72b9
Razor-eng <rajatmaharana64@gmail.com>
2024-05-06 15:37:49 +0530

    first commit

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -10,9 +10,9 @@ const Projects = () => {
     return (
         <div className="px-3 md:px-28 py-2 z-10 overflow-hidden min-h-screen max-w-screen dark:text-white">
             <div className="flex w-full justify-end">
-                <div className="w-44 flex items-center px-2 py-1 rounded-xl bg-rose-50 focus-within:w-full md:focus-within:w-72 focus-within:outline focus-within:outline-blue-600 transition-all ease-in duration-200">
+                <div className="w-44 flex items-center px-2 py-1 rounded-xl bg-zinc-50 dark:bg-zinc-700 focus-within:w-full md:focus-within:w-72 focus-within:outline focus-within:outline-blue-600 transition-all ease-in duration-200">
                     <FaSearch className="text-zinc-400" />
-                    <input type="text" className="w-full bg-transparent pl-1 outline-none placeholder-zinc-400 dark:text-black" placeholder="Search Projects" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+                    <input type="text" className="w-full bg-transparent pl-1 outline-none placeholder-zinc-400 dark:text-white" placeholder="Search Projects" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
                 </div>
             </div>
             {
